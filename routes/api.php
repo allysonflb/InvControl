69c626efe02e3972f02dd143a731d374be44fd8a
Thales-Avagnina <thalesavagnina22016@gmail.com>
2024-10-22 16:14:26 -0300

    Corrigindo um pequeno erro no nome da class e routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductManagerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::put('/products/{id}', [ProductManagerController::class, 'update']);
+Route::delete('/products/{id}', [ProductManagerController::class, 'destroy']);
 require __DIR__.'/productmanager.php';
