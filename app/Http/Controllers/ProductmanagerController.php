e8576231dbfb9ac4849594e6883921cc93620255
Allyson Freire <allysonflb@msn.com>
2024-10-21 21:18:09 -0300

    Adicao de validacao de json e save data no create

--- app/Http/Controllers/ProductmanagerController.php
+++ app/Http/Controllers/ProductmanagerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\View\View;
+use illuminate\Support\Facades\Validator;
 use App\Models\Products;
 
 
@@ -19,13 +19,37 @@ class ProductManagerController extends Controller
 
     public function produtosCreate(Request $request): JsonResponse
     {
-        $produto = new Products();
-        $produto->nome = $request->nome;
-        $produto->preco = $request->preco;
-        $produto->quantidade = $request->quantidade;
-        $produto->descricao = $request->descricao;
-        $produto->save();
-        return response()->json(['Produto criado:' => $produto]);
+        // Validação dos dados enviados
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'preco' => 'required|string',
+            'quantidade' => 'required|numeric|min:0',
+            'descricao' => 'required|integer|min:1',
+        ]);
+
+        // Se a validação falhar, retornar um erro
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error na validação do json',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        try {
+            $produto = new Products();
+            $produto->nome = $request->nome;
+            $produto->preco = $request->preco;
+            $produto->quantidade = $request->quantidade;
+            $produto->descricao = $request->descricao;
+            $produto->save();
+            return response()->json(['Produto criado:' => $produto]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Falha ao salvar produto.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function produtosUpdate(Request $request, $id): JsonResponse
