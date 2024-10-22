9d435b7cbd7a8235f35c0487d634edbe1fb3ddd8
johnzin12 <johnalancunha@gmail.com>
2024-10-22 12:34:52 -0300

    john

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'description' => 'nullable|string',
+        ]);
+
+        $product = Product::create($validatedData);
+
+        return response()->json($product, 201);
+    }
+}
+
+
